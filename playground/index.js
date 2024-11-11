0a7ff0617755146978f71edd678e061345cd8045
Viktor Zdorovtsev <v.zdvacew@gmail.com>
2024-11-11 11:46:38 +0700

    chore: update playground

--- playground/index.js
+++ playground/index.js
@@ -5,6 +5,6 @@ export default async function (fastify, _opts) {
     token: '',
   })
 
-  // fastify.grammy.start()
-  fastify.grammy.command('start', ctx => ctx.reply('Hi, I\'m run faster than you :)'))
+  fastify.grammy.command('start', ctx => ctx.react('😍'))
+  fastify.grammy.on('message', ctx => ctx.react('👍'))
 }
